814b34ea9bf9b9a92a3a10912d02c97a4c5b952a
Maldestor95 <mMaldestor95@gmail.com>
2024-12-12 14:15:42 +0100

    feat(minesweeper): ongoing

--- src/pages/minesweeper/gridmanager.ts
+++ src/pages/minesweeper/gridmanager.ts
@@ -1,80 +1,119 @@
-export default class GRIDMANAGER{
+type mineCell = {
+    land: "M" | " " | number
+    discovery: "Hidden" | "Discovered" | "Flagged"
+}
+
+export default class GRIDMANAGER {
     private nbCols: number
     private nbRows: number
-    minesFields: Array<Array<string|number>>
-    
-    constructor(option:{cols:number,rows:number,nbMines:number})
-    {
-        this.nbCols=option.cols
-        this.nbRows=option.rows
-        this.minesFields=Array.from(Array(this.nbRows),()=>new Array(this.nbCols))
+    minesFields: mineCell[][]
+
+    constructor(option: { cols: number, rows: number, nbMines: number }) {
+        this.nbCols = option.cols
+        this.nbRows = option.rows
+        this.minesFields = this.creatEmptyGrid()
         this.prepareMineFields(option.nbMines)
-        
-    }
+        // this.minesFields[5][5].land = "M"
+        // this.minesFields[9][2].land = "M"
+        // this.minesFields[0][1].land = "M"
+        // this.setNumberOfneighbours({ l: 4, c: 4 })
 
-    prepareMineFields(nbmines:number)
-    {
-        for (let index = 0; index < nbmines; index++) {
-            const newMine= this.placeOneMine()
-            this.minesFields[newMine.position.x][newMine.position.y]="M"           
+    }
+    private creatEmptyGrid() {
+        const grid = []
+        for (let index = 0; index < this.nbRows; index++) {
+            const rows = []
+            for (let index = 0; index < this.nbCols; index++) {
+                const emptyCell: mineCell = { land: " ", discovery: "Hidden" }
+                rows.push(emptyCell)
+            }
+            grid.push(rows)
         }
-    
-        return this.minesFields
+        return grid
+    }
+    prepareMineFields(nbMines: number): void {
+
+        for (let index = 0; index < nbMines; index++) {
+            const newMine = this.placeOneMine()
+            this.minesFields[newMine.position.x][newMine.position.y].land = "M"
+        }
+
+        for (let rr = 0; rr < this.nbRows; rr++) {
+            for (let cc = 0; cc < this.nbCols; cc++) {
+                if (this.minesFields[rr][cc].land != 'M') this.minesFields[rr][cc].land = this.getNumberOfneighbours({ l: rr, c: cc })
+            }
+        }
 
     }
-    
-    private placeOneMine(){
-        const randomPosition= Math.floor( Math.random()*this.nbCols*this.nbRows)
-        const mineX=Math.floor(randomPosition/this.nbCols) 
-        const mineY=randomPosition-mineX*this.nbCols
+
+    private placeOneMine() {
+        const randomPosition = Math.floor(Math.random() * this.nbCols * this.nbRows)
+        const mineX = Math.floor(randomPosition / this.nbCols)
+        const mineY = randomPosition - mineX * this.nbCols
 
         return {
-            position: { x: mineX, y: mineY }, 
-            isPlacementValid: this.isPlacementValid({'c':mineX,'l':mineY-1})
-            }
+            position: { x: mineX, y: mineY },
+            // isPlacementValid: this.isPlacementValid({'c':mineX,'l':mineY-1})
+        }
     }
 
-    isPlacementValid(position:{l:number,c:number}){
+    isPlacementValid(position: { l: number, c: number }) {
         const MaximumSurrounding = 4
 
-        let numberOfSurroundingMines=0
+        let numberOfSurroundingMines = 0
 
 
-        let LsearchOffset:number[]=[-1,0,1]
+        let LsearchOffset: number[] = [-1, 0, 1]
         let CsearchOffset: number[] = [-1, 0, 1]
-        
-        if (position.l>this.nbRows-2 ) LsearchOffset=[-1,0]
-        if (position.l<1) LsearchOffset=[0,1]
-   
-        if (position.c>this.nbCols-2) CsearchOffset=[-1,0]
-        if (position.c<1) CsearchOffset=[0,1]
 
-            
+        if (position.l > this.nbRows - 2) LsearchOffset = [-1, 0]
+        if (position.l < 1) LsearchOffset = [0, 1]
 
-        const minesFields= this.minesFields
-        CsearchOffset.forEach(y=>{
-            LsearchOffset.forEach(x=>{
+        if (position.c > this.nbCols - 2) CsearchOffset = [-1, 0]
+        if (position.c < 1) CsearchOffset = [0, 1]
+
+
+
+        const minesFields = this.minesFields
+        CsearchOffset.forEach(y => {
+            LsearchOffset.forEach(x => {
                 try {
                     // console.log("ici", position,x,y,minesFields[position.l + x ][position.c + y ])
 
-                    const mineFound = minesFields[position.l + x][position.c + y]=="M"
-                    if (mineFound ) numberOfSurroundingMines = numberOfSurroundingMines + 1
+                    // const mineFound = minesFields[position.l + x][position.c + y]=="M"
+                    // if (mineFound ) numberOfSurroundingMines = numberOfSurroundingMines + 1
                 } catch (error) {
-                    console.log("error", position, x, y, this.nbRows, this.nbCols,  LsearchOffset, CsearchOffset,)
+                    console.log("error", position, x, y, this.nbRows, this.nbCols, LsearchOffset, CsearchOffset,)
                 }
-                })
             })
+        })
 
         return numberOfSurroundingMines
     }
 
-    fillwithneighbours (){
-        for (let rr = 0; rr < this.nbRows; rr++) {
-            for (let cc = 0; cc < this.nbCols; cc++) {
-                if (this.minesFields[rr][cc]!="M") 
-                    this.minesFields[rr][cc]=this.isPlacementValid({l:rr,c:cc})
-            }
-        }
+    getNumberOfneighbours(position: { l: number, c: number, }): number {
+        const boundaries = [
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [0, -1],
+            [0, 1],
+            [1, -1],
+            [1, 0],
+            [1, 1],
+        ]
 
+        let counter = 0
+        boundaries.forEach(neighbour => {
+            try {
+                if (this.minesFields[position.l + neighbour[0]][position.c + neighbour[1]].land == 'M') counter += 1;
+            } catch (error) {
+
+            }
+
+        })
+        return counter
     }
+
+
 }
--- src/pages/minesweeper/index.vue
+++ src/pages/minesweeper/index.vue
@@ -2,21 +2,14 @@
     <div>
 minesweeper
 <p> table Size{{ tablesize }}</p>
-<button @click="newGame">New</button>
+<p>{{ Mine.minesFields[ligne][colonne] }}</p>
+<p>{{ ligne}}/{{colonne }}</p>
+<p>{{ kaboom }}</p>
+<button @click="newGame" class="text-black">New</button>
 
-<div class="container shadow-xl py-4 my-4 ">
-    <div class="text-black grid grid-cols-4 ">
-        <div>Columns</div>
-        <input type="number" name="col" v-model="colonne" min="0" :max="grid.minesFields[0].length">
-        <div>Row</div>
-        <input type="number" name="row" v-model="ligne" min="0" :max="grid.minesFields.length">
-    </div>
-    <div class="flex gap-4 flex-row">
-    <div>Surroundings</div>
-    <div>{{ neighbours }}</div>
-    </div>
+<div v-if="kaboom">
+    <img src="mine.svg" alt="" class="absolute w-28 bg-red-600" >
 </div>
-
 
 <div class="w-80 mx-auto">
 
@@ -27,10 +20,20 @@ minesweeper
         </div>
     </div>
 
-    <div v-for="(row,row_id) in Mine" :key="row_id" class="grid grid-cols-11 h-8">
+    <div v-for="(row,row_id) in Mine.minesFields" :key="row_id" class="grid grid-cols-11 h-8">
         <div>{{ row_id }}</div>
-        <div v-for="(item, item_id) in row" key:="item_id" class="border-green-400 border-[1px] border-solid w-8" @click="setdata(row_id,item_id)" >
-            {{ item==0?"":item }}
+        <div v-for="(item, item_id) in row" key:="item_id" class="border-green-400 border-[1px] border-solid w-8 cursor-pointer" 
+        @click.left="setdata(row_id,item_id) "
+        @click.right.prevent="setflag( row_id,item_id)" 
+        >
+            <div v-if="item.discovery=='Hidden'" class="bg-slate-300 w-8 h-8"> </div>
+            <div v-if="item.discovery=='Flagged'" class="w-8 h-8"> 
+                <img src="flag.svg" alt="M" class="w-4 mx-auto py-2">
+            </div>
+            <div v-if="item.discovery=='Discovered'" >
+                <img v-if="item.land=='M'" src="mine.svg" alt="M" class="w-4 mx-auto py-2">
+                <div v-if="item.land!=0 && item.land!='M'">{{ item.land }}</div>
+            </div>
         </div>   
     </div>
 
@@ -40,32 +43,100 @@ minesweeper
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
+import { computed, nextTick, onMounted, ref } from 'vue';
 import gridmanager from './gridmanager'
 
+
 const tablesize=ref({x:10,y:20})
-const grid=new gridmanager({cols:tablesize.value.x,rows:tablesize.value.y,nbMines:30})
-const Mine=ref()
+const grid=new gridmanager({cols:tablesize.value.x,rows:tablesize.value.y,nbMines:10})
+const Mine=ref(grid)
 const colonne=ref(0)
 const ligne=ref(0)
+const kaboom=ref(false)
 
 const neighbours= computed(()=>{
     return grid.isPlacementValid({c:colonne.value,l:ligne.value})
 })
-const setdata=(row:number,col:number)=>{
+const setdata=async(row:number,col:number)=>{
     colonne.value=col
     ligne.value=row
+    if (Mine.value.minesFields[row][col].discovery!='Flagged') 
+    {
+        Mine.value.minesFields[row][col].discovery="Discovered"
+        discoverNeighbours(row,col)
+        if (Mine.value.minesFields[row][col].land=='M') kaboom.value=true
+    }
+    await nextTick()
+
 }
-onMounted(()=>{
-    grid.fillwithneighbours()
-    Mine.value=grid.minesFields
-})
-const newGame=()=>{
-    const grid=new gridmanager({cols:tablesize.value.x,rows:tablesize.value.y,nbMines:30})
-    Mine.value=grid.minesFields
-    grid.fillwithneighbours()
-    Mine.value=grid.minesFields
+const setflag= (row:number,col:number)=>{
+    colonne.value=col
+    ligne.value=row
+    
+    switch (Mine.value.minesFields[row][col].discovery) {
+        case 'Flagged':
+            Mine.value.minesFields[row][col].discovery='Hidden'
+            break;
+        case 'Hidden':
+            Mine.value.minesFields[row][col].discovery='Flagged'
+            break;               
+        default:
+            break;
+    }
+    
 }
+
+const discoverNeighbours=(row:number,col:number)=>{
+    
+        
+        // look at 8 surroundings,  if value is 0,  perform setdata function on the cell to carry on the discovery
+        const boundaries = [
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [0, -1],
+            [0, 1],
+            [1, -1],
+            [1, 0],
+            [1, 1],
+        ]
+        
+        boundaries.forEach(neighbour => {
+            try {
+                const currentNeighbour= Mine.value.minesFields[row+neighbour[0]][col+neighbour[1]]
+                if (currentNeighbour.discovery=="Hidden") 
+                {
+                    if (currentNeighbour.land==0){
+                        {               
+                            currentNeighbour.discovery="Discovered"
+                            discoverNeighbours(row + neighbour[0],col + neighbour[1])
+                            boundaries.forEach(freeNeighbour=>{
+                                const visibleNeighbour= Mine.value.minesFields[row+freeNeighbour[0]][col+freeNeighbour[1]]
+                                if (visibleNeighbour.land!="M") visibleNeighbour.discovery="Discovered"
+                            })
+                        }            
+                    }
+                }
+            } catch (error) {
+                // console.log(error)
+            }
+            
+        })
+    
+        
+    }
+    
+    onMounted(()=>{
+        // grid.fillwithneighbours()
+        newGame()
+    })
+    const newGame=()=>{
+        const grid=new gridmanager({cols:tablesize.value.x,rows:tablesize.value.y,nbMines:30})
+        Mine.value=grid
+        kaboom.value=false
+        // grid.fillwithneighbours()
+        // Mine.value=grid
+    }
 </script>
 
 <style scoped>
